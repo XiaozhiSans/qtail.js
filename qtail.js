0d99e057fe93216ccde8181c58366145305f9d9b
XiaozhiSans <xiaozhisans@qq.com>
2024-05-22 13:33:25 +0800

    v1.7.0-stable
    [x] 生成尾巴逻辑重写

--- qtail.js
+++ qtail.js
@@ -1,51 +1,58 @@
 const qtail = {
 	appName: "qtail.js",
 	appNameShort: "qtail",
-	version: "v1.6.2",
-	versionCode: 16120240424,
-	buildVer: "(20240424)",
+	version: "v1.7.0",
+	buildVer: "(20240520)",
 	buildType: "Stable",
-	license: "MIT License",
+	license: "MIT",
 	author: "XiaozhiSans",
 	url: "https://github.com/XiaozhiSans/qtail",
-	getVer: function() {
-		console.log("qtail.js version: " + qtail.buildType + ' ' + qtail.version + qtail.buildVer);
+	getVer: () => {
+		console.log(`[qtail.js] version: ${qtail.buildType} ${qtail.version}${qtail.buildVer}`);
 		return qtail.version;
 	},
 	generation: function(name, tail) {
-		console.log("正在生成新的昵称 (￣﹃￣)");
-		tail = qtail.rstr(tail);
-		console.log("尾巴重整完毕 (￣﹃￣) 结果为: " + tail);
-		var result = name + String.fromCharCode(8295) + tail + String.fromCharCode(8294);
+		console.log("[qtail.js] 正在生成新的昵称 (￣﹃￣)");
+		// tail = qtail.rstr(tail);
+		tail = this.retail(tail);
+		console.log("[qtail.js] 尾巴重整完毕 (￣﹃￣) 结果为: " + tail);
+		let result = name + String.fromCharCode(8295) + tail + String.fromCharCode(8294);
 
-		console.log("新昵称生成完毕 (￣﹃￣) 结果为: " + result);
+		console.log("[qtail.js] 新昵称生成完毕 (￣﹃￣) 结果为: " + result);
 
 		return result;
 	},
-	rstr: function(str) {
-		for (var i = 33; i <= 47; i++) { // ! - /
-			var symbol = String.fromCharCode(i);
+	retail: tail => {
+		let symbols = tail.replace(/[\u4e00-\u9fa5]|[\u4e00-\u9fa5]|[a-zA-Z]|[0-9]/g, '');
+		if(!symbols || symbols == '') {return tail;}
+		let name = eval(`tail.replace(/${symbols}/g, '');`);
+		let result = symbols + name;
+		return result;
+	},
+	/* rstr: function(str) {
+		for (let i = 33; i <= 47; i++) { // ! - /
+			let symbol = String.fromCharCode(i);
 			if (str.endsWith(symbol)) {
 				str = symbol + str.split(symbol).join("");
 				return str;
 			}
 		}
-		for (var i = 58; i <= 65; i++) { // : - @
-			var symbol = String.fromCharCode(i);
+		for (let i = 58; i <= 65; i++) { // : - @
+			let symbol = String.fromCharCode(i);
 			if (str.endsWith(symbol)) {
 				str = symbol + str.split(symbol).join("");
 				return str;
 			}
 		}
-		for (var i = 91; i <= 96; i++) { // [ - `
-			var symbol = String.fromCharCode(i);
+		for (let i = 91; i <= 96; i++) { // [ - `
+			let symbol = String.fromCharCode(i);
 			if (str.endsWith(symbol)) {
 				str = symbol + str.split(symbol).join("");
 				return str;
 			}
 		}
-		for (var i = 123; i <= 191; i++) { // { - ¿
-			var symbol = String.fromCharCode(i);
+		for (let i = 123; i <= 191; i++) { // { - ¿
+			let symbol = String.fromCharCode(i);
 			if (str.endsWith(symbol)) {
 				str = symbol + str.split(symbol).join("");
 				return str;
@@ -53,17 +60,13 @@ const qtail = {
 		}
 		return str;
 		// return str.split('').reverse().join('');
-	}
+	} */
 }
 
 console.log(
 	"\n" +
-		" %c " + qtail.appName + ' ' + qtail.version + " by " + qtail.author  +
-		" %c License: " + qtail.license +
-		" %c " + qtail.url +
+		" %c " + `${qtail.appName} ${qtail.version} ${qtail.buildType} by ${qtail.author} `+
 		"\n" +
 	"\n",
-	"color: #fff; background: #fd79a8; padding: 5px 0;",
-	"color: black; background-color: #999; padding: 5px 0;",
-	"background: #FFF; padding: 5px 0;"
+	"color: #fff; background: #fd79a8; padding: 5px 0;"
 );
--- readme.md
+++ readme.md
@@ -2,50 +2,37 @@
   
 如何使用:  
 #### 1.html
-```html
-<!-- 任选其一 -->
+```html index.html
 <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/XiaozhiSans/qtail.js/qtail.js"></script>
-<script type="text/javascript" src="https://xiaozhisans.github.io/qtail/js/qtail.js"></script>
-<script type="text/javascript" src="https://raw.githubusercontent.com/XiaozhiSans/qtail.js/main/qtail.js"></script>
-```  
-#### 2.npm
-```bat
-npm i XiaozhiSans/qtail.js
-```  
-  
-### 可用函数
-```js
-qtail.getVer(); // 在控制台输出 qtail.js 完整信息,并返回 qtail.js 版本号,示例返回值: "v1.6.1"
-qtail.generation(name, tail); // 生成带有小尾巴的昵称,示例返回值: "name⁧tail⁦"
-qtail.rstr(str); // 对字符串进行重整,示例返回值: "str"
-```  
-  
-### 扩展模块
-+ qtail.js html 模块:
-  - 引入 html:
-  > **注**:你可以通过[qtailLoader](https://github.com/XiaozhiSans/qtailLoader)实现自动化加载.
-    ```html
-    <!-- 任选其一 -->
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/XiaozhiSans/qtail/js/qtail.html.js"></script>
-    <script type="text/javascript" src="https://xiaozhisans.github.io/qtail/js/qtail.html.js"></script>
-    <script type="text/javascript" src="https://raw.githubusercontent.com/XiaozhiSans/qtail/main/js/qtail.html.js"></script>
-    ```  
-  - 热键说明:
-    1. 回车(Enter): 执行函数 `qtailHtml.main();`
-    2. 退出(Esc): 执行函数 `qtailHtml.exit();`
-    3. 8号功能键(F8): 执行函数 `qtailHtml.copy();`
-  
-  - 可用函数:
-  ```js
-  qtailHtml.main(); // 从 html 页面拉取 "#qtail" 表单的内容并调用 qtail.generation(); 生成昵称并返回 html 页面带有 "#result" 的元素
-  qtailHtml.copy(); // 将 html 页面上带有 "#result" 的元素的内容复制到剪贴板
-  qtailHtml.share(); // 分享页面
-  qtailHtml.donation(); // 喜欢页面
-  qtailHtml.exit(); // 询问是否要退出页面
-  ```  
-  
-## 协议
 ```
+
+
+#### 2.npm
+```bash
+npm i XiaozhiSans/qtail.js
+```
+
+
+### 可用函数
+```js functions.js
+qtail.getVer(); // 在控制台输出 qtail.js 完整版本信息,并返回 qtail.js 版本号,示例返回值: "v1.6.1"
+qtail.generation(name, tail); // 生成带有小尾巴的昵称,示例返回值: "name⁧tail⁦"
+qtail.retail(tail); // 对尾巴进行重整,示例返回值: "tail"
+
+/*
+ * 过时函数
+ * 它们曾经存在过,现在已被取代
+ */
+qtail.rstr(str); // 对字符串进行重整,示例返回值 "str"
+```
+
+
+### 扩展模块
++ [qtail.js html 模块](https://xiaozhisans.github.io/qtail/js/qtail.html.js)
+
+
+## 协议
+```txt LICENSE.txt
 MIT License
  
 Copyright (c) 2024 XiaozhiSans
@@ -68,4 +55,3 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ```
-  
